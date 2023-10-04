cad0f7b936ad4cba16fef6a5b01c55eecb099ddf
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-04 01:09:01 +0100

    remove char

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    try:
+        if n < 0:
+            return str
+        c = ""
+        n = int(n)
+        for i in str:
+            if i == str[n]:
+                i = ""
+            c += i
+        return c
+    except:
+        IndexError
+        return str
+    
