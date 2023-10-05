6dddeb817f1980752b8352d5e7a7fe5b93b71bf8
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-05 23:50:59 +0100

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(j))
     for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[1]))
+        print("{}: {}".format(i, sys.argv[i]))
