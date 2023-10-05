e22ec276e780303be23558196f62611e1e7bbfde
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-05 19:14:57 +0100

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ import add_0
 if __name__ == "__main__":
     a = 1
     b = 2
-    add = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, add))
+    #add = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
