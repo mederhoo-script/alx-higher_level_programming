21e3ba3c5a3cd4696dc5051d26834182a4a7ea56
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-05 23:23:56 +0100

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
 
     def args_print():
-        j = len(argv) - 1
+        j = len(sys.argv) - 1
         if j == 1:
             print("{} argument:".format(j))
         elif j == 0:
             print("{} arguments.".format(j))
         else:
             print("{} arguments:".format(j))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[1]))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[1]))
 
     args_print()
