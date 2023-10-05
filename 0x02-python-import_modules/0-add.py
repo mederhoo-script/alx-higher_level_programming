037c3817a9e4e408e5cd94bd665c7feb9ae5ac2d
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-05 19:23:14 +0100

    add two digit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
-    
+    from add_0 import add
+
     a = 1
     b = 2
-    add = add_0.add(a, b)
+    add = add(a, b)
     print("{} + {} = {}".format(a, b, add))
