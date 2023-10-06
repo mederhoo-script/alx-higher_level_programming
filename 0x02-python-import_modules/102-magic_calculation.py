eb4750e388347f3760b73ebbd46085bb81b25d05
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-06 01:31:42 +0100

    magic cal

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,6 @@
-def magic_calculation(a, b):
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def magic_calculation(a, b):
     add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
 
     if a < b:
