754547164d89fe821d926b0f6ad264b79693b7bf
hammed amusat oriyomi <mederhoo@gmail.com>
2023-10-10 15:49:24 -0700

    no more C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 def no_c(my_string):
     output = ""
     for i in my_string:
-        if i == 'c' or i == 'C':
+        if i == 'c':
+            i = ""
+        elif i == 'C':
             i = ""
         output += i
