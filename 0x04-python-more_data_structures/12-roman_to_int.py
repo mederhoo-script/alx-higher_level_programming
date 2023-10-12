5176635ac6a1a779c9bc3c9c325d3227189e77be
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-12 23:20:07 +0100

    roman to digit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    try:
+        rodic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        preva = 0
+
+        for c in roman_string:
+            current_value = rodic[c]
+            if current_value > preva:
+                result += current_value - 2 * preva
+            else:
+                result += current_value
+            preva = current_value
+
+        return result
+    except (TypeError, KeyError):
+        return 0
