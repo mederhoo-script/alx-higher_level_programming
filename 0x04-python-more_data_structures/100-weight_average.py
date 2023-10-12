97113aa70689a13335cb20e7045111e7833b45c0
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-12 23:31:06 +0100

    average weight

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,12 +2,12 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    
+
     total_weighted_sum = 0
     total_weight = 0
-    
+
     for score, weight in my_list:
         total_weighted_sum += score * weight
         total_weight += weight
-    
+
     return total_weighted_sum / total_weight
