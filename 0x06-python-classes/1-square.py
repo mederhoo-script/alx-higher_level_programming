3e9d921dcbc362ceb00cf19d1e32c640536bde33
mederhoo <mederhoo@mederhoo.kali.com>
2023-10-25 01:42:58 +0100

    squre

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
