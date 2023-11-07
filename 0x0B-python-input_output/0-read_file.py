cbbab4302e5c8689397ddfc4c3258b6b8a282067
mederhoo <mederhoo@mederhoo.kali.com>
2023-11-07 19:22:18 +0100

    read line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pythone3
 
 def read_file(filename=""):
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding='utf-8') as file:
         output = file.read()
         return output
