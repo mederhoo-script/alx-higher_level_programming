adc625e756edf62f4faa39843ba556f982023731
mederhoo <mederhoo@mederhoo.kali.com>
2023-11-07 20:39:36 +0100

    read line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/pythone3
-"""a function that reads a text file (UTF8) and prints it to stdout:"""
+"""define a function that reads a text file (UTF8) and prints it to stdout:"""
+
 
 def read_file(filename=""):
     """
-    Read the content of a file and return it as a string.
-    
-    Args:
-        filrname: the name the file to read.
+Read the content of a file and return it as a string.
+
+Args:
+    filrname: the name the file to read.
 
     Return:
         str: the file content as string
@@ -17,14 +18,7 @@ def read_file(filename=""):
     E.g:
         >>> read_file("tests/my_file_0.txt")
         We offer a truly innovative approach to education:
-focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers. 
-
-A school every software engineer would have dreamt of!
-guillaume@ubuntu:~/0x0B$ ./0-main.py
-We offer a truly innovative approach to education:
-focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers. 
-
-A school every software engineer would have dreamt of!
+focus on building reliable applications and scalable s
     """
     with open(filename, "r", encoding='utf-8') as file:
         output = file.read()
