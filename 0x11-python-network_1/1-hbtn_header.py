dcfad4a90d69163fc1e79ac4f858488c8079d681
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-01-26 17:33:13 +0100

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -18,6 +18,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as rep:
-        html_rep = rep.read()
-x_id = rep.headers.get("X-Request-Id")
-print(x_id)
+        x_id = rep.headers.get("X-Request-Id")
+        print(x_id)
