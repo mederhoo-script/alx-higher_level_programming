9b33f51a83c28908b49deb8ac4a00096df57596b
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-01-26 15:45:48 +0100

    what is my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status
+
+    You must use the package urllib
+    You are not allowed to import any packages other than urllib
+    The body of the response must be displayed like the following example (tabulation before -)
+    You must use a with statement
+"""
+
+
+if __name__ == "__main__":
+	import urllib.request
+
+	with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+		html_content = response.read()
+	print("Body response:")
+	print("\t- type: {}".format(type(html_content)))
+	print(f"\t- content: {html_content}")
+	print(f"\t- utf8 content: {html_content.decode('utf-8')}")
