95dfbf13ed02e03f58d2ac851f2626086015375d
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-20 19:46:33 +0100

    fs in js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+let fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		console.log(data)
+	}
+});
